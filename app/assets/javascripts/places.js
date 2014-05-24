091800eb0922d6d3d900f5d142ac878fb7087ee6
Johnathon <ja.lueth@gmail.com>
2014-05-23 23:25:41 -0500

    mobile menu, need to render  instead of page loads

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -11,4 +11,19 @@ $(document).ready(function() {
     cyclic: true,
   });
 
+  $(".glass.full-menu").click(function(){
+    console.log("click");
+    $("#sidebar ul").slideToggle("slow",function(){
+      $(".glass.empty-menu").css({"display" : "block"});
+      $(".glass.full-menu").css({"display" : "none" });
+      $("#sidebar ul").css({"display" : "none"});
+    })
+  })
+  $(".glass.empty-menu").click(function(){
+    $("#sidebar ul").slideToggle("slow",function(){
+      $(".glass.empty-menu").css({"display" : "none"});
+      $(".glass.full-menu").css({"display" : "block" });
+      $("#sidebar ul").css({"display" : "block"});
+  })
+})
 });
